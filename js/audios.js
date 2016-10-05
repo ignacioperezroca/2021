db83ca39e5ba1829921ab6356c1253190539c52b
User\Ignacio <ignacio.perezroca@gmail.com>
2016-10-05 15:51:39 -0300

    20-08

--- js/audios.js
+++ js/audios.js
@@ -1,68 +1,69 @@
 	var lastAudio = null;
 	var audio = null;
 	var arrayAudio = [
-		{url:'_materiales/00-ferran.mp3', isRange:false, isPlaying:false, audio:null},
-		{url:'_materiales/01-kim.wav', isRange:false, isPlaying:false, audio:null},
-		{url:'_materiales/02-altea.mp3', isRange:false, isPlaying:false, audio:null},
-		{url:'_materiales/03-lila.mp3', isRange:false, isPlaying:false, audio:null},
-		{url:'_materiales/04-mefisto.mp3', isRange:false, isPlaying:false, audio:null},
-		{url:'_materiales/05-lodi.mp3', isRange:false, isPlaying:false, audio:null},
-		{url:'_materiales/06-reznik.mp3', isRange:false, isPlaying:false, audio:null},
-		{url:'_materiales/07-completadoelproceso.mp3', isRange:true, idRange:'range-0', isPlaying:false, audio:null},
-		{url:'_materiales/08-juegoterminado.mp3', isRange:true, idRange:'range-1', isPlaying:false, audio:null},
-		{url:'_materiales/09-alertaevento.mp3', isRange:true, idRange:'range-2', isPlaying:false, audio:null},
-		{url:'_materiales/10-findelprotocolo.mp3', isRange:true, idRange:'range-3', isPlaying:false, audio:null},
-		{url:'_materiales/11-estasconectada.mp3', isRange:true, idRange:'range-4', isPlaying:false, audio:null},
-		{url:'_materiales/12-asd.wav', isRange:true, idRange:'range-5', isPlaying:false, audio:null},
-		{url:'_materiales/13-cambiandoacontrolmanual.wav', isRange:true, idRange:'range-6', isPlaying:false, audio:null},
-		{url:'_materiales/14-asd.wav', isRange:true, idRange:'range-7', isPlaying:false, audio:null}
+		{url:'audios/00-ferran.mp3', isRange:false, isPlaying:false, audio:null},
+		{url:'audios/01-kim.wav', isRange:false, isPlaying:false, audio:null},
+		{url:'audios/02-altea.mp3', isRange:false, isPlaying:false, audio:null},
+		{url:'audios/03-lila.mp3', isRange:false, isPlaying:false, audio:null},
+		{url:'audios/04-mefisto.mp3', isRange:false, isPlaying:false, audio:null},
+		{url:'audios/05-lodi.mp3', isRange:false, isPlaying:false, audio:null},
+		{url:'audios/06-reznik.mp3', isRange:false, isPlaying:false, audio:null},
+		{url:'audios/07-completadoelproceso.mp3', isRange:true, idRange:'range-0', isPlaying:false, audio:null},
+		{url:'audios/08-juegoterminado.mp3', isRange:true, idRange:'range-1', isPlaying:false, audio:null},
+		{url:'audios/09-alertaevento.mp3', isRange:true, idRange:'range-2', isPlaying:false, audio:null},
+		{url:'audios/10-findelprotocolo.mp3', isRange:true, idRange:'range-3', isPlaying:false, audio:null},
+		{url:'audios/11-estasconectada.mp3', isRange:true, idRange:'range-4', isPlaying:false, audio:null},
+		{url:'audios/12-asd.wav', isRange:true, idRange:'range-5', isPlaying:false, audio:null},
+		{url:'audios/13-cambiandoacontrolmanual.wav', isRange:true, idRange:'range-6', isPlaying:false, audio:null},
+		{url:'audios/14-asd.wav', isRange:true, idRange:'range-7', isPlaying:false, audio:null}
 	];
 
 	(function(){
 		$('.play-audio').click(function(){
 
+
+			/*si ya hay un audio cargado y es diferente de lo que estamos
+			 tocando ahora
+			*/
 			if(lastAudio != $(this).attr('data-position') && lastAudio !=null){
+					
+					/*volvo a pregunta si exite el audio y si esta tocando lo pauso*/
 					if(arrayAudio[lastAudio].audio != null && !arrayAudio[lastAudio].audio.paused){
 							arrayAudio[lastAudio].audio.pause();
-							arrayAudio[lastAudio].audio.currentTime = 0;
+							/*guardo el nuevo audio*/
 							lastAudio = $(this).attr('data-position');
+							/*play audio*/
 							clickAudio($(this).attr('data-position'), $(this).attr('data-range'));
 					}else{
+						/**/
 						lastAudio = $(this).attr('data-position');
 						clickAudio($(this).attr('data-position'), $(this).attr('data-range'));
-
 					}
-			}else{
-				  	lastAudio = $(this).attr('data-position');
-						clickAudio($(this).attr('data-position'), $(this).attr('data-range'));
+					return;
 			}
-			
-			setTimeout(function(){
-				if(isPlaying=true){
-					stopAudio();
-					// audio.src = '';
-					console.log(stopAudio);
-				}
-			}
-			,1000);
 
+			/*pero si el audio no esta vacio y es lo mismo*/
+			if(lastAudio != null ){
+					if(!arrayAudio[lastAudio].audio.paused){
+						arrayAudio[lastAudio].audio.play()
+					}else{
+						arrayAudio[lastAudio].audio.pause()
+					}	  	
+			}else{
+			  /*sino lo creo*/
+				lastAudio = $(this).attr('data-position');
+				clickAudio($(this).attr('data-position'), $(this).attr('data-range'));
+			}
 		});
 	})();
-
-	function stopAudio(){
-		audio.pause();
-	}
-	function playAudio(){
-		audio.play();
-	}
 
 	function clickAudio(posAudio, rangeTrue){
 		var range;
 		if(posAudio){
   		arrayAudio[posAudio].isPlaying = true;
 
- 			 audio = document.createElement('audio');
- 			 arrayAudio[posAudio].audio = audio; 
+ 			audio = document.createElement('audio');
+ 			arrayAudio[posAudio].audio = audio; 
 
  			audio.src = arrayAudio[posAudio].url;
  			var idRange = 'range-' + posAudio;
@@ -83,7 +84,7 @@
 							range.value = Math.floor(parseFloat((audio.currentTime )  * 100)).toFixed(0)
 						}
 		  			if(!audio.paused){
-		  				time();
+		  				  time();
 		  			}else{
 		  					arrayAudio[posAudio].isPlaying = false;
 		  			}
